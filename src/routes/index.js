5bb5556f206f377414e8436f7c710d20a2f3d4e7
Jonas <jonaslagoteixeira@gmail.com>
2020-03-31 15:16:53 -0300

    feito redirecionamento de rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+
+      <Route path="/" component={() => <h1>404</h1>} />
     </Switch>
   );
 }
