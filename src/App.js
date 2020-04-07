0fc8c3bf93e5daf3101ec5f7316ba9623d6c6cbe
Jonas <jonaslagoteixeira@gmail.com>
2020-04-07 17:56:51 -0300

    feita a gravacao da sessao do usuario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/ReactotronConfig';
 
 import { Router } from 'react-router-dom';
 import history from './services/history';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
@@ -15,10 +16,12 @@ import Routes from './routes';
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
